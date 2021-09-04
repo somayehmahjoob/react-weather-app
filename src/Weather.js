d70f2b1003daac60cafaccc2c3eb670db3170611
Somayeh mahjoob <somayehmahjob@gmail.com>
2021-09-04 15:05:55 +0100

    [Add] left side

---
+++ src/Weather.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { IconContext } from "react-icons";
+import { FaSearchLocation, FaWind, FaCloudversify } from 'react-icons/fa';
+import cloudy from './cloudy.png';
+
+import './App.css';
+
+
+export default function Weather(){
+  return(
+    <div className="row content">
+        <div className="col-md-3 p-0">
+          <div className="search-form">
+            <form action="#">
+              <i className="fas fa-search"></i>
+              <input type="search" placeholder="Search for place.." />
+              <button id="submitButton">
+                <IconContext.Provider value={{ className: "weather-icon" }}>
+                  <FaSearchLocation />
+                </IconContext.Provider>            
+              </button>
+            </form>
+          </div>
+          <div className="weather-now">
+            <div className="weather-icon-now">
+              <img src={cloudy} alt="cloudy" id="weather-icon-now" />              
+            </div>
+            <div className="weather-temp-now">12°<span>C</span></div>
+            <div className="weather-date-now">
+              <span id="weather-day">Monday,</span
+              ><span id="weather-time">16:00</span>
+              <hr />
+              <div className="weather-description">
+                <IconContext.Provider value={{ className: "weather-icon" }}>
+                  <FaCloudversify />
+                </IconContext.Provider>                
+                <span id="weather-description">Mosley cloudy</span>
+              </div>
+              <div className="weather-wind">                
+                <IconContext.Provider value={{ className: "weather-icon" }}>
+                  <FaWind />
+                </IconContext.Provider>
+                <span id="weather-wind">Wind</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-9">ddddddd</div>
+      </div>
+  
+  )
+  
+}
