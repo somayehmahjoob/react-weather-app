0fd410bce9c6c11bc44a15b50b99ee344a9ddb82
Somayeh mahjoob <somayehmahjob@gmail.com>
2021-09-09 17:20:03 +0100

    [Add] Footer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-        <Weather cityName="ahwaz"/>
-      </div>      
+        <Weather cityName="ahwaz" />
+      </div>
+      <div className="footer">
+        This project was coded by Somayeh.Mahjoob and is{" "}
+        <a href="https://github.com/somayehmahjoob/SearchEngine-React">
+          Open-source code on GitHub
+        </a>
+        , and Hosted on and{" "}
+        <a href="https://lucid-agnesi-49a3e5.netlify.app/">
+          hosted on Netlify{" "}
+        </a>
+      </div>
     </div>
   );
 }
